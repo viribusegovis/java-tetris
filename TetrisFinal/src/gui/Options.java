78ddb4986e88c7bfb52a0c98056e3ccd0da77bad
Bruno Freitas <est.bruno.freitas@pt.softinsa.com>
2023-05-15 15:35:17 +0100

    GUI Test

--- TetrisFinal/src/gui/Options.java
+++ TetrisFinal/src/gui/Options.java
@@ -8,12 +8,12 @@ package gui;
  *
  * @author viribusegovis
  */
-public class MainMenu extends javax.swing.JFrame {
+public class Options extends javax.swing.JFrame {
 
     /**
-     * Creates new form MainMenu
+     * Creates new form Options
      */
-    public MainMenu() {
+    public Options() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class MainMenu extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Options.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Options.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Options.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainMenu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Options.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new MainMenu().setVisible(true);
+                new Options().setVisible(true);
             }
         });
     }
